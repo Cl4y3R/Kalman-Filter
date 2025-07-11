5e3931333a2fc529ace420e5949e7276d52fe7e4
Cl4y3R <49401622+Cl4y3R@users.noreply.github.com>
2025-07-11 11:10:42 +0800

    Update ekfCore.hpp

--- ekfCore.hpp
+++ ekfCore.hpp
@@ -8,11 +8,14 @@ public:
     EKF(const Eigen::VectorXd &x_in, const Eigen::MatrixXd &S_in, const Eigen::MatrixXd &Q, const Eigen::MatrixXd &R, const double &dt, const bool &num_opt_flag = false);
     ~EKF();
 
-    void predict(Eigen::VectorXd (*func)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps);
-    void predict(const Eigen::VectorXd &u, const Eigen::MatrixXd &F_);
-    void update(Eigen::VectorXd (*func)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps, const Eigen::VectorXd &z);
-    void update(const Eigen::VectorXd &z, const Eigen::MatrixXd &H_);
-    void setJacobian(const Eigen::MatrixXd &F_in, const Eigen::MatrixXd &H_in);
+    void run(Eigen::VectorXd (*stateFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        Eigen::VectorXd (*measureFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps, const Eigen::VectorXd &z);
+    void run(Eigen::VectorXd (*stateFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        Eigen::VectorXd (*measureFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps, const Eigen::VectorXd &z,
+        const Eigen::MatrixXd &F_in, const Eigen::MatrixXd &H_in);
+
     Eigen::MatrixXd numJacobian(Eigen::VectorXd (*func)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&), const Eigen::VectorXd &x, const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double &h);
     Eigen::VectorXd getEstimation();
     Eigen::MatrixXd qrFactor(const Eigen::MatrixXd &A, const Eigen::MatrixXd &S, const Eigen::MatrixXd &Ns);
@@ -77,92 +80,10 @@ EKF::~EKF(){
     std::cout<<"EKF destructed"<<std::endl;
 }
 
-void EKF::predict(Eigen::VectorXd (*func)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps){
-    // numerical jacobian method, jacobian matrix calculated externally every step
-    if(init_flag_){
-        dx_ = func(x_, u, param);
-        x_ = x_ + dx_ * dt_;
-        if (num_opt_flag_ == true){
-            S_ = qrFactor(F_, S_, Rq_);
-        }
-    }
-    else{
-        init_flag_ = true;
-    }
-}
-
-void EKF::predict(const Eigen::VectorXd &u, const Eigen::MatrixXd &F_in){
-    // pre-calculated explicit jacobian
-    if(init_flag_){
-        F_ = F_in;
-        dx_ = F_ * x_;
-        x_ = x_ + dx_ * dt_;
-        if (num_opt_flag_ == true){
-            S_ = qrFactor(F_, S_, Rq_);
-        }
-    }
-    else{
-        init_flag_ = true;
-    }
-}
-
-void EKF::update(Eigen::VectorXd (*func)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps, const Eigen::VectorXd &z){
-    // numerical jacobian method, jacobian matrix calculated externally every step
-    Eigen::MatrixXd K;
-    Eigen::MatrixXd P;
-    if (num_opt_flag_ == true){
-        Eigen::MatrixXd Sy = qrFactor(H_, S_, Rr_);
-        P = S_* S_.transpose();
-        Eigen::MatrixXd Pxy = P * H_.transpose();
-        K = Pxy * ((Sy * Sy.transpose()).inverse());
-    }
-    else{
-        P = F_ * S_ * F_.transpose() + Rq_;
-        K = P * H_.transpose() * ((H_ * P * H_.transpose() + Rr_).inverse());
-    }
-    
-    Eigen::VectorXd y = func(x_, u, param);
-    x_ = x_ + K * (z - y);
-    Eigen::MatrixXd A = Eigen::MatrixXd::Identity(x_.size(), x_.size()) - K * H_;
-    if (num_opt_flag_ == true){
-        S_ = qrFactor(A, S_, K * Rr_);
-    }
-    else{
-        S_ = A * P;
-    }   
-}
-
-void EKF::update(const Eigen::VectorXd &z, const Eigen::MatrixXd &H_in){
-    // pre-calculated explicit jacobian
-    H_ = H_in;
-    Eigen::MatrixXd K;
-    Eigen::MatrixXd P;
-    if (num_opt_flag_ == true){
-        Eigen::MatrixXd Sy = qrFactor(H_, S_, Rr_);
-        P = S_* S_.transpose();
-        Eigen::MatrixXd Pxy = P * H_.transpose();
-        K = Pxy * ((Sy * Sy.transpose()).inverse());
-    }
-    else{
-        P = F_ * S_ * F_.transpose() + Rq_;
-        K = P * H_.transpose() * ((H_ * P * H_.transpose() + Rr_).inverse());
-    }
-    
-    Eigen::VectorXd y = H_ * x_;
-    x_ = x_ + K * (z - y);
-    Eigen::MatrixXd A = Eigen::MatrixXd::Identity(x_.size(), x_.size()) - K * H_;
-    if (num_opt_flag_ == true){
-        S_ = qrFactor(A, S_, K * Rr_);
-    }
-    else{
-        S_ = P;
-    }   
-}
-
 Eigen::MatrixXd EKF::numJacobian(Eigen::VectorXd (*func)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&), const Eigen::VectorXd &x, const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double &h){
     Eigen::VectorXd Z = func(x, u, param);
     Eigen::MatrixXd J = Eigen::MatrixXd::Zero(Z.size(), x.size());
-    for(int i = 0; i < Z.size(); i++){
+    for(int i = 0; i < x.size(); i++){
         Eigen::VectorXd x_plus_h = x;
         double eps = std::max(h, h*std::fabs(x_plus_h(i)));
         x_plus_h(i) += eps;
@@ -186,10 +107,95 @@ Eigen::MatrixXd EKF::qrFactor(const Eigen::MatrixXd &A, const Eigen::MatrixXd &S
     return R.transpose();
 }
 
-void EKF::setJacobian(const Eigen::MatrixXd &F_in, const Eigen::MatrixXd &H_in){
-    F_ = F_in;
-    H_ = H_in;
-}
 Eigen::VectorXd EKF::getEstimation(){
     return x_;
+}
+
+void EKF::run(Eigen::VectorXd (*stateFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        Eigen::VectorXd (*measureFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps, const Eigen::VectorXd &z){
+    // numerical jacobian method, jacobian matrix calculated every step
+    F_ = numJacobian(stateFunc, x_, u, param, eps);
+    H_ = numJacobian(measureFunc, x_, u, param, eps);
+    Eigen::MatrixXd K;
+    Eigen::MatrixXd P;
+    if (num_opt_flag_ == true){
+        // propagate upper triangle state covariance matrix
+        if (init_flag_ == true){
+            x_ = stateFunc(x_, u, param);
+            S_ = qrFactor(F_, S_, Rq_);
+        }
+        else{
+            init_flag_ = true;
+        }
+        P = S_* S_.transpose();
+        Eigen::VectorXd y = measureFunc(x_, u, param);
+        Eigen::MatrixXd Sy = qrFactor(H_, S_, Rr_);
+        Eigen::MatrixXd Pxy = P * H_.transpose();
+        K = Pxy * ((Sy * Sy.transpose()).inverse());
+        Eigen::MatrixXd A = Eigen::MatrixXd::Identity(x_.size(), x_.size()) - K * H_;
+        S_ = qrFactor(A, S_, K * Rr_);
+        x_ = x_ + K * (z - y);
+    }
+    else{
+        // propagate full state covariance matrix
+        if (init_flag_ == true){
+            dx_ = stateFunc(x_, u, param);
+            P = F_ * S_ * F_.transpose() + Rq_;
+        }
+        else{
+            P = S_;
+            init_flag_ = true;
+        }
+        Eigen::VectorXd y = measureFunc(x_, u, param);
+        K = P * H_.transpose() * ((H_ * P * H_.transpose() + Rr_).inverse());
+        Eigen::MatrixXd A = Eigen::MatrixXd::Identity(x_.size(), x_.size()) - K * H_;
+        S_ = A * P;
+        x_ = x_ + K * (z - y);
+    }
+}
+
+void EKF::run(Eigen::VectorXd (*stateFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        Eigen::VectorXd (*measureFunc)(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&),
+        const Eigen::VectorXd &u, const Eigen::VectorXd &param, const double eps, const Eigen::VectorXd &z,
+        const Eigen::MatrixXd &F_in, const Eigen::MatrixXd &H_in){
+    // jacobian matrix calculated externally
+    F_ = F_in;
+    H_ = H_in;
+    Eigen::MatrixXd K;
+    Eigen::MatrixXd P;
+    if (num_opt_flag_ == true){
+        // propagate upper triangle state covariance matrix
+        if (init_flag_ == true){
+            x_ = stateFunc(x_, u, param);
+            S_ = qrFactor(F_, S_, Rq_);
+        }
+        else{
+            init_flag_ = true;
+        }
+        P = S_* S_.transpose();
+        Eigen::VectorXd y = measureFunc(x_, u, param);
+        Eigen::MatrixXd Sy = qrFactor(H_, S_, Rr_);
+        Eigen::MatrixXd Pxy = P * H_.transpose();
+        K = Pxy * ((Sy * Sy.transpose()).inverse());
+        Eigen::MatrixXd A = Eigen::MatrixXd::Identity(x_.size(), x_.size()) - K * H_;
+        S_ = qrFactor(A, S_, K * Rr_);
+        x_ = x_ + K * (z - y);
+    }
+    else{
+        // propagate full state covariance matrix
+        if (init_flag_ == true){
+            x_ = stateFunc(x_, u, param);
+            P = F_ * S_ * F_.transpose() + Rq_;
+        }
+        else{
+            P = S_;
+            init_flag_ = true;
+        }
+        Eigen::VectorXd y = measureFunc(x_, u, param);
+        K = P * H_.transpose() * ((H_ * P * H_.transpose() + Rr_).inverse());
+        Eigen::MatrixXd A = Eigen::MatrixXd::Identity(x_.size(), x_.size()) - K * H_;
+        S_ = A * P;
+        x_ = x_ + K * (z - y);
+    }
 }
